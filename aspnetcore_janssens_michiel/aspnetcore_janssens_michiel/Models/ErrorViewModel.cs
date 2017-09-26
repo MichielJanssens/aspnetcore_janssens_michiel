6a70011e0b0286210e5c9a59dca3aeb335f425f8
MichielJanssens <michiel.janssens88@gmail.com>
2017-09-26 18:49:02 +0200

    Empty installation

---
+++ aspnetcore_janssens_michiel/aspnetcore_janssens_michiel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspnetcore_janssens_michiel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
